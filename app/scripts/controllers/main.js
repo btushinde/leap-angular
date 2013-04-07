5491da93db635726d0f82ea557682ebb979b30c1
Biko Tushinde <biko@smashingboxes.com>
2013-04-06 22:45:37 -0400

    leap service now forwards gesture events

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,26 @@
 'use strict';
 
 angular.module('leapDemo')
-  .controller('MainCtrl', function ($scope) {
-    
+  .controller('MainCtrl', function ($scope, $rootScope, Leap) {
+	// Start Leap controller
+	Leap.init();
+
+
+	$rootScope.$on('leap.circle', function(){
+		console.log('CIRCLE ROOT SCOPE');
+	});
+
+	$rootScope.$on('leap.screenTap', function(){
+		console.log('SCREENTAP ROOT SCOPE');
+	});
+
+
+	$rootScope.$on('leap.keyTap', function(){
+		console.log('key TAP ROOT SCOPE');
+	});
+
+	$rootScope.$on('leap.swipe', function(){
+		console.log('Swipe ROOT SCOPE');
+	});
+	
   });
--- app/scripts/services/Leap.js
+++ app/scripts/services/Leap.js
@@ -1,37 +1,23 @@
 'use strict';
 
 angular.module('leapDemo')
-  .factory('Leap', function () {
-    var leap = {};
+	.factory('Leap', function ($rootScope) {
+		var leap = {};
 
-    leap.init = function(){
-      var controller = new Leap.Controller({enableGestures: true})
-        .loop(function(frame) {
-          $('div').css('background', 'wheat');
-          if (frame.gestures.length > 0) {
-            for (var i = 0; i < frame.gestures.length; i++) {
-              var g = frame.gestures[i]; 
+		leap.init = function(){
+			var controller = new Leap.Controller({enableGestures: true})
+				.loop(function(frame) {
 
-              $('.' + g.type ).css('background', 'red');
+					if (frame.gestures.length > 0) {
+						for (var i = 0; i < frame.gestures.length; i++) {
+							var g = frame.gestures[i]; 
 
-              if (g.type == 'circle') {
-                console.log('swipe!');
+							$rootScope.$emit('leap.' + g.type, g);
+						}
+					}
+			});
+		};
 
-              }else if ( g.type == 'swipe' ){
-                console.log('swipe!');
-
-              }else if ( g.type == 'screenTap' ){
-                console.log('screenTap!');
-
-              }else if ( g.type == 'keyTap' ){
-                console.log('keyTap!');               
-              }
-
-            }
-          }
-      });
-    };
-
-    // Public API here
-    return leap;
-  });
+		// Public API here
+		return leap;
+	});
